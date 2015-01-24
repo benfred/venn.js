f4d1bd5b7e7bab5896b1f230cf23b11d7db60262
Ben Frederickson <ben@zite.com>
2015-01-24 09:39:48 -0800

    fix layout issue + minor perfomance optimizations
    Greedy layout of completely overlapped circles was sometimes not working
    correctly, fixed and add a unittest.
    
    Also change fmin to not allocate vectors inside the loop, instead allocate
    outside and reuse.

--- venn.js
+++ venn.js
@@ -50,7 +50,7 @@
     have the overlap area 'overlap' */
     venn.distanceFromIntersectArea = function(r1, r2, overlap) {
         // handle complete overlapped circles
-        if (Math.min(r1, r2) * Math.min(r1,r2) * Math.PI <= overlap) {
+        if (Math.min(r1, r2) * Math.min(r1,r2) * Math.PI <= overlap + SMALL) {
             return Math.abs(r1 - r2);
         }
 
@@ -341,14 +341,6 @@
             });
     }
 
-    function weightedSum(a, b) {
-        var ret = new Array(a[1].length || 0);
-        for (var j = 0; j < ret.length; ++j) {
-            ret[j] = a[0] * a[1][j] + b[0] * b[1][j];
-        }
-        return ret;
-    }
-
     /** finds the zeros of a function, given two starting points (which must
      * have opposite signs */
     venn.bisect = function(f, a, b, parameters) {
@@ -382,6 +374,13 @@
         return a + delta;
     };
 
+
+    function weightedSum(ret, w1, v1, w2, v2) {
+        for (var j = 0; j < ret.length; ++j) {
+            ret[j] = w1 * v1[j] + w2 * v2[j]
+        }
+    }
+
     /** minimizes a function using the downhill simplex method */
     venn.fmin = function(f, x0, parameters) {
         parameters = parameters || {};
@@ -394,7 +393,8 @@
             chi = parameters.chi || 2,
             psi = parameters.psi || -0.5,
             sigma = parameters.sigma || 0.5,
-            callback = parameters.callback;
+            callback = parameters.callback,
+            temp;
 
         // initialize simplex.
         var N = x0.length,
@@ -410,6 +410,11 @@
 
         var sortOrder = function(a, b) { return a.fx - b.fx; };
 
+        var centroid = x0.slice(),
+            reflected = x0.slice(),
+            contracted = x0.slice(),
+            expanded = x0.slice();
+
         for (var iteration = 0; iteration < maxIterations; ++iteration) {
             simplex.sort(sortOrder);
             if (callback) {
@@ -421,7 +426,6 @@
             }
 
             // compute the centroid of all but the worst point in the simplex
-            var centroid = new Array(N);
             for (i = 0; i < N; ++i) {
                 centroid[i] = 0;
                 for (var j = 0; j < N; ++j) {
@@ -433,17 +437,21 @@
             // reflect the worst point past the centroid  and compute loss at reflected
             // point
             var worst = simplex[N];
-            var reflected = weightedSum([1+rho, centroid], [-rho, worst]);
+            weightedSum(reflected, 1+rho, centroid, -rho, worst);
             reflected.fx = f(reflected);
-
-            var replacement = reflected;
 
             // if the reflected point is the best seen, then possibly expand
             if (reflected.fx <= simplex[0].fx) {
-                var expanded = weightedSum([1+chi, centroid], [-chi, worst]);
+                weightedSum(expanded, 1+chi, centroid, -chi, worst);
                 expanded.fx = f(expanded);
                 if (expanded.fx < reflected.fx) {
-                    replacement = expanded;
+                    temp = simplex[N];
+                    simplex[N] = expanded;
+                    expanded = temp;
+                }  else {
+                    temp = simplex[N];
+                    simplex[N] = reflected;
+                    reflected = temp;
                 }
             }
 
@@ -455,19 +463,23 @@
 
                 if (reflected.fx <= worst.fx) {
                     // do an inside contraction
-                    contracted = weightedSum([1+psi, centroid], [-psi, worst]);
+                    weightedSum(contracted, 1+psi, centroid, -psi, worst);
                     contracted.fx = f(contracted);
                     if (contracted.fx < worst.fx) {
-                        replacement = contracted;
+                        temp = simplex[N];
+                        simplex[N] = contracted;
+                        contracted = temp;
                     } else {
                         shouldReduce = true;
                     }
                 } else {
                     // do an outside contraction
-                    contracted = weightedSum([1-psi * rho, centroid], [psi*rho, worst]);
+                    weightedSum(contracted, 1-psi * rho, centroid, psi*rho, worst);
                     contracted.fx = f(contracted);
                     if (contracted.fx <= reflected.fx) {
-                        replacement = contracted;
+                        temp = simplex[N];
+                        simplex[N] = contracted;
+                        contracted = temp;
                     } else {
                         shouldReduce = true;
                     }
@@ -476,14 +488,16 @@
                 if (shouldReduce) {
                     // do reduction. doesn't actually happen that often
                     for (i = 1; i < simplex.length; ++i) {
-                        simplex[i] = weightedSum([1 - sigma, simplex[0]],
-                                                 [sigma - 1, simplex[i]]);
+                        weightedSum(simplex[i], 1 - sigma, simplex[0], sigma - 1, simplex[i]);
                         simplex[i].fx = f(simplex[i]);
                     }
                 }
+            } else {
+                temp = simplex[N];
+                simplex[N] = reflected;
+                reflected = temp;
             }
 
-            simplex[N] = replacement;
         }
 
         simplex.sort(sortOrder);
